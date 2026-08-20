2b3cf44e0efdac7b71e31952e92e562bc4e6b443
aliabdelazim7 <1.23182953e+08+aliabdelazim7@users.noreply.github.com>
2026-08-20 09:55:50 +0000

    fix: explicitly register ViewServiceProvider in api/main.php to resolve build('view') error

--- config/view.php
+++ config/view.php
@@ -1,34 +1,8 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | View Storage Paths
-    |--------------------------------------------------------------------------
-    |
-    | Most templating systems load templates from disk. Here you may specify
-    | an array of paths that should be checked for your views.
-    |
-    */
-
     'paths' => [
-        resource_path('views'),
+        base_path('resources/views'),
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Compiled View Path
-    |--------------------------------------------------------------------------
-    |
-    | Vercel's serverless filesystem is read-only except for /tmp. The
-    | production vercel.json sets VIEW_COMPILED_PATH=/tmp.
-    |
-    */
-
-    'compiled' => env(
-        'VIEW_COMPILED_PATH',
-        realpath(storage_path('framework/views'))
-    ),
-
+    'compiled' => env('VIEW_COMPILED_PATH', '/tmp/views'),
 ];
