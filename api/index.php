1a1f86e42cb7bb97041fb8f7f026a2a989938882
aliabdelazim7 <1.23182953e+08+aliabdelazim7@users.noreply.github.com>
2026-08-20 03:13:09 +0000

    fix: pass api path through vercel function

--- api/index.php
+++ api/index.php
@@ -18,4 +18,16 @@ if (is_string($forwardedPath) && str_starts_with($forwardedPath, '/api')) {
     $_SERVER['QUERY_STRING'] = http_build_query($query);
 }
 
+$currentUri = $_SERVER['REQUEST_URI'] ?? '/';
+$currentPath = parse_url($currentUri, PHP_URL_PATH) ?: '/';
+$entrypointPrefix = '/api/index.php/';
+
+if (str_starts_with($currentPath, $entrypointPrefix)) {
+    $laravelPath = '/api'.substr($currentPath, strlen('/api/index.php'));
+    $queryString = $_SERVER['QUERY_STRING'] ?? '';
+
+    $_SERVER['REQUEST_URI'] = $laravelPath.($queryString !== '' ? '?'.$queryString : '');
+    $_SERVER['PATH_INFO'] = $laravelPath;
+}
+
 require __DIR__.'/../public/index.php';
