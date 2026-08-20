2c70625c8cc0009a80a735612bb5862765966a9d
aliabdelazim7 <1.23182953e+08+aliabdelazim7@users.noreply.github.com>
2026-08-20 09:30:57 +0000

    fix: final entry point move and vercel routing

--- index.php
+++ index.php
@@ -22,8 +22,8 @@ if ($isApi) {
 
     define('LARAVEL_START', microtime(true));
 
-    require __DIR__.'/../vendor/autoload.php';
-    $app = require_once __DIR__.'/../bootstrap/app.php';
+    require __DIR__.'/vendor/autoload.php';
+    $app = require_once __DIR__.'/bootstrap/app.php';
     $response = $app->handleRequest(\Illuminate\Http\Request::capture());
     $response->send();
     $app->terminate($response);
@@ -31,7 +31,7 @@ if ($isApi) {
 }
 
 // Load manifest to get hashed asset paths
-$manifestPath = __DIR__.'/../public/build/manifest.json';
+$manifestPath = __DIR__.'/public/build/manifest.json';
 $assets = [
     'css' => [],
     'js' => ''
